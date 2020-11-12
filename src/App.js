830f6a3410824d88de6f89f113193058a12cd941
muslimkazmi <irsayyadreza@outlook.com>
2020-11-12 14:51:35 +0330

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Lrnreact from "./Lrnreact.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Lrnreact Sub1="React" Sub2="HTML" />
+
+      <hr />
+
+      <Lrnreact Sub1="Angular" Sub2="CSS" />
+
+      <hr />
+
+      <Lrnreact Sub1="Vue" Sub2="JAVASCRIPT" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Lrnreact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Lrnreact(props) {
+  return (
+    <div>
+      <h1> I learned How to Install {props.Sub1} </h1>
+
+      <h1> I learned {props.Sub2} </h1>
+    </div>
+  );
+}
+
+export default Lrnreact;
